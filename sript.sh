cbbad3cd7e1a5b158e105928c710c2429ac7b62f
thomasj79 <thomasjones79@gmail.com>
2014-09-26 15:44:50 +0100

    changed file to executable script

--- sript.sh
+++ sript.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+#: Title : Grep command
+#: Date : 26/9/14
+#: Author : Thomas Jones
+#: Version : 1.0000000005
+#: Description : CPU count script
+#: Options : No
+
 cat /proc/cpuinfo | grep processor | wc -l
